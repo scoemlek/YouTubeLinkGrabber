08ec5fd2f8e5eb7a6ec795efe20ff8be3858f470
SCOE <100028991+scoemlek@users.noreply.github.com>
2024-09-30 16:29:09 +0200

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -5,9 +5,16 @@ import os
 def grab(url):
     response = requests.get(url, timeout = 15).text
     if '.m3u8' not in response:
-        print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
         return
+        
     end = response.find('.m3u8') + 5
+    if (end < 0):
+        print("")
+        return
+
+    end = end + 5
+    print(end)
+    
     tuner = 100
     while True:
         if 'https://' in response[end - tuner : end]:
@@ -19,18 +26,7 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
-with open('./youtubeLink.txt', encoding='utf-8') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('##'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('-')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}", {ch_name}')
-        else:
-            grab(line)
+grab(line)
             
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
