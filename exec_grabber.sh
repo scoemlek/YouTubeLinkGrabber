9201800203a8f0b23e8a5ead8d3d8e83f6394040
SCOE <100028991+scoemlek@users.noreply.github.com>
2024-10-02 21:53:02 +0200

    Update exec_grabber.sh

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 -m pip install requests
 
-python3 YouTubeLinkGrabber.py > ./youtube.m3u
+#python3 YouTubeLinkGrabber.py > ./youtube.m3u
+
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=o35l3S6_2h8 > ./atv.m3u8
 
 echo M3U grabbed.
