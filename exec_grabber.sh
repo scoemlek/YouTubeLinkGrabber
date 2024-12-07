b7171959aca403f830c40ca8d9526a990a26c6f7
SCOE <scoemlek@users.noreply.github.com>
2024-12-07 12:44:06 +0100

    ..is updated.

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -8,17 +8,16 @@ python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=82O6yOy_XwE > ./Eu
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=ouuCjEjyKVI > ./ShowTV.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=XnvS-RZa4Qw > ./ShowTurk.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Y3vGHFrsqrs > ./ShowMax.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=n6BJbjjBmW8 > ./EuroD.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=6wHAK439FDI > ./EuroD.m3u8
 
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=lf1NxAexRAE > ./CennetMahallesi.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=z2SSpfbknj8 > ./YeniGelin.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=hfx8H7YrmTw > ./KemalSunalGulsah.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=-p70_GmE9og > ./ZekiMetinTV.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=rMjgoq_tMPE > ./YesilcamArzu.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=VF2zggx54Hs > ./TAFFPictures.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=wi75yRkoK4A > ./YesilcamArzu.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Hc-2ShK4YHI > ./TAFFPictures.m3u8
 
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=BJ3Yv572V1A > ./NatGeoWild.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=TBF-fYm-cTI > ./Kabe.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=0-ULTdNUtDo > ./NatGeoWild.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=zvkKWcbcZ38 > ./Kabe.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=4XtE9TdzN2Q > ./TgrtEU.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=ay5qBS0PUg4 > ./SemerkandTV.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=6qtUZDWaOjo > ./TRTNostalji.m3u8
