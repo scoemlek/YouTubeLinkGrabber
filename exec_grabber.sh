da7da0d42bc6bea3227f956757e06e3233e5c7bc
SCOE <scoemlek@users.noreply.github.com>
2025-03-21 23:13:18 +0100

    ..is updated.

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install requests
+#python3 -m pip install requests
 
 #python3 YouTubeLinkGrabber.py > ./youtube.m3u
 
@@ -10,19 +10,19 @@ python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=XnvS-RZa4Qw > ./Sh
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Y3vGHFrsqrs > ./ShowMax.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=6wHAK439FDI > ./EuroD.m3u8
 
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=lf1NxAexRAE > ./CennetMahallesi.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=-75iiMuNWxc > ./YeniGelin.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@cennetmahallesishowtv/live > ./CennetMahallesi.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@yenigelindizi/live > ./YeniGelin.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=hfx8H7YrmTw > ./KemalSunalGulsah.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=UgKxh04Iv9o > ./YesilcamArzu.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Hc-2ShK4YHI > ./TAFFPictures.m3u8
 
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=oPCwpbLLPTo > ./NatGeoWild.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=wek2Nh_XeoY > ./Kabe.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86_%D8%A7%D9%84%D9%83%D8%B1%D9%8A%D9%85/live > ./Kabe.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Gq20kW8WsU0 > ./TgrtEU.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@semerkandtv/live > ./SemerkandTV.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=ftfJHeVJbek > ./ibo.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=DbQ4HGgr7Xo > ./ntv.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=nmY9i63t6qo > ./aHaber.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@Ahaber/live > ./aHaber.m3u8
 
 python3 ../MügeAnliGrabber.py
 
