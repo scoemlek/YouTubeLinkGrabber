9fbd9273f18d637d253de5f5bac1048149ec4538
SCOE <scoemlek@users.noreply.github.com>
2025-04-08 09:00:32 +0200

    ..is updated.

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -18,12 +18,13 @@ python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Hc-2ShK4YHI > ./TA
 
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@NationalGeographicEspanol/live > ./NatGeoWild.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86_%D8%A7%D9%84%D9%83%D8%B1%D9%8A%D9%85/live > ./Kabe.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=Gq20kW8WsU0 > ./TgrtEU.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@tgrteutv/live > ./TgrtEU.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@semerkandtv/live > ./SemerkandTV.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=ftfJHeVJbek > ./ibo.m3u8
-python3 YouTubeLinkGrabber.py https://www.youtube.com/watch?v=DbQ4HGgr7Xo > ./ntv.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@turkfilmleri8452/live > ./ibo.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/ntv/live > ./ntv.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@Ahaber/live > ./aHaber.m3u8
 python3 YouTubeLinkGrabber.py https://www.youtube.com/@CocuklarDuymasnKanalD/live > ./duymasin.m3u8
+python3 YouTubeLinkGrabber.py https://www.youtube.com/@cnnturk/live > ./cnn.m3u8
 
 #python3 ../MügeAnliGrabber.py
 
